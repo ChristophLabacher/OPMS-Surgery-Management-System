3646aa2c8fa769feec2dabde9feb4ba9a33bd034
Christoph Labacher <christoph.labacher@gmail.com>
2015-02-26 14:33:24 +0100

    Scrolling, scales move, main-side expand

---
+++ scripts/viewScheduleAction.js
@@ -0,0 +1,17 @@
+$(document).ready(function()	{
+	
+	/************************
+	*	Expand MAIN-SIDE	*
+	************************/
+	$("[action = 'expand-center-side']").click(function()	{	
+		var target = $(this).closest(".view").find(".center-side");
+			
+		if (target.hasClass("active"))	{
+
+		} else	{
+			
+		}
+		
+		target.toggleClass("active");
+	});
+});
--- scripts/viewScheduleSetup.js
+++ scripts/viewScheduleSetup.js
@@ -1,7 +1,38 @@
 $(document).ready(function() {
+	
+	// Add guidlines for half hours
 	$(".column-main").each(function()	{
 		for(var i = 0; i < 47; i++)	{
 			$(this).append("<hr style=\"top: " + (i * 60 + 60) + "px \">");
 		}
 	});
+	
+	// Setup scrolling
+	myScroll = new IScroll('#timetable-scrollable', {
+		scrollX: true,
+		scrollY: true,
+		
+		mouseWheel: true,
+		scrollbars: true,
+		keyBindings: true,
+		
+		click: true,
+		disableMouse: true,
+		freeScroll: true,
+		momentum: false,
+		bounce: false,
+		
+		probeType: 2
+		
+	});
+	
+	myScroll.on('scroll', function()	{
+		console.log(this.x + " - " + this.y);
+		
+		$(".schedule .scale-time").css("transform", "translateY(" + this.y + "px)");
+		$(".schedule .scale-rooms").css("transform", "translateX(" + this.x + "px)");
+
+	});
+
+	
 });
