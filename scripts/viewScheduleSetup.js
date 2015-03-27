f07e1da8bfcb5bba746dd68dd7b33c4dd227a789
Christoph Labacher <christoph.labacher@gmail.com>
2015-03-27 14:24:53 +0100

    Load cards locally and add data corresponding to size

--- scripts/viewScheduleSetup.js
+++ scripts/viewScheduleSetup.js
@@ -17,4 +17,69 @@ $(document).ready(function() {
 		$(".schedule .scale-rooms").css("transform", "translateX(" + x + "px)");		
 	});
 	
+	loadFile();
+	getCards();
 });
+
+function loadFile()	{
+	// Get json-file
+	var request = new XMLHttpRequest();
+		request.open("GET", "../data/surgeries.json", false);
+		request.send(null);
+	// Put json-file into an array
+	data = JSON.parse(request.responseText);
+}
+
+function getCards()	{
+	for (var i = 0; i < data.length; i++)	{
+		var Starttime = new Date(data[i].Starttime)
+		var Endtime = new Date(data[i].Endtime)
+
+		var top = (Starttime.getUTCHours() + Starttime.getUTCMinutes()/60) * 120;
+		var height = ((Endtime.getUTCHours() + Endtime.getUTCMinutes()/60) - (Starttime.getUTCHours() + Starttime.getUTCMinutes()/60)) * 120;
+
+		var card =	"<div class=\"card-container\" style=\"top:" + top + "px;\">" +
+						"<div class=\"card\" style=\"height: " + height + "px;\">" +
+							"<div class=\"handle handle-top\"></div><!-- handle-top -->" +
+							
+							"<div class=\"header state-" + data[i].Current_State + "\">";
+		
+		if (parseInt(data[i].Current_State) >= 8)	{
+			card +=				"<img src=\"imgs/assets/icons/surgery.png\">";
+		}
+		
+		 card +=				"<p class=\"name\">" + data[i].Name + ", " + data[i].Prename + "</p>" +
+								"<p class=\"birthdate\">" + data[i].Birthdate + "</p>" +
+							"</div><!-- header -->";
+							
+		if (height >= 120)	{
+			card += "<div class=\"main\">" +
+						"<p class=\"diagnose\">" + data[i].Diagnosis + "</p>";
+						
+			if (height >= 150)	{
+				card +=	"<p class=\"team\">OP: " + data[i].Surgery_Team + "</p>";
+			}
+			
+			card +=		"</div><!-- main -->";
+		}
+							
+		if (height >= 180)	{
+			card += "<div class=\"footer\">" +
+						"<p class=\"room\"> Saal" + data[i].Surgery_Room + "</p>" +
+						"<p class=\"time\">" + addZero(Starttime.getUTCHours()) + ":" + addZero(Starttime.getUTCMinutes()) + " – " + addZero(Endtime.getUTCHours()) + ":" + addZero(Endtime.getUTCMinutes()) + "</p>" +
+					"</div><!-- footer -->";
+		}
+																			
+		card +=				"<div class=\"handle handle-bottom\"></div><!-- handle-bottom -->" +
+					
+						"</div><!-- card -->" +
+					"</div><!-- card-container -->"
+		
+		$(".column[room='" + data[i].Surgery_Room + "'] .column-main").prepend(card);
+
+	}	
+}
+		
+function addZero(num){
+	return (num<10?"0":"")+num;
+}
