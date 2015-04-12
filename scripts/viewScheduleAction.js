5b2e767511ddb4d1651684ca9526d4bd99af844f
Christoph Labacher <christoph.labacher@gmail.com>
2015-04-12 15:17:25 +0200

    Start next State via Button or List

--- scripts/viewScheduleAction.js
+++ scripts/viewScheduleAction.js
@@ -57,6 +57,16 @@ $(document).ready(function()	{
 			addEditability(target);
 			$(this).attr("src", "imgs/assets/icons/confirm.png");
 		}
+
+		if (target.hasClass("timestamps"))	{
+			if (target.hasClass("editing"))	{
+				setTimeout(function(){
+					target.find(".next").attr("value", "Starten");
+				}, 300)
+			} else	{
+				target.find(".next").attr("value", "");
+			}
+		}
 		
 		target.toggleClass("editing");
 	});
@@ -64,7 +74,7 @@ $(document).ready(function()	{
 	/************************
 	*Update Barchart-Legend	*
 	************************/
-	$(".column-details .barchart-bars div[start]").mouseover(function()	{
+	$(".column-details .barchart-bars").on("mouseover", "div[start]", function()	{
 		$(this).parent().find(".active").removeClass("highlight");
 		
 		var startTime = $(this).attr("start");
@@ -79,7 +89,7 @@ $(document).ready(function()	{
 			print = "Seit " + startTime;
 		}
 			
-		print += " · " + states[state].title;
+		print += " · " + states[state-1].title;
 		
 		var legendClass = "text-state-" + state;
 		
@@ -89,12 +99,12 @@ $(document).ready(function()	{
 		target.text(print);
 	})
 	
-	$(".column-details .barchart-bars div.next").mouseover(function()	{
+	$(".column-details .barchart-bars").on("mouseover", "div.next", function()	{
 		$(this).parent().find(".active").removeClass("highlight");
 
 		var state = $(this).attr("state");
 		
-		var print = "„" + states[state].title + "“ jetzt starten";
+		var print = "„" + states[state-1].title + "“ jetzt starten";
 		
 		var legendClass = "text-state-" + state;
 		
@@ -104,14 +114,14 @@ $(document).ready(function()	{
 		target.text(print);
 	});
 	
-	$(".column-details .barchart-bars").mouseout(function()	{
+	$(".column-details").on("mouseout", ".barchart-bars", function()	{
 		var source = $(this).closest(".barchart").find(".barchart-bars div.active");
 		source.addClass("highlight");
 		
 		var startTime = $(source).attr("start");
 		var state = $(source).attr("state");
 		
-		var print = "Seit " + startTime +  " · " + states[state].title;
+		var print = "Seit " + startTime +  " · " + states[state-1].title;
 		
 		var legendClass = "text-state-" + state;
 		
@@ -120,6 +130,60 @@ $(document).ready(function()	{
 		target.removeClass().addClass("barchart-legend").addClass(legendClass);
 		target.text(print);
 	});
+	
+	/************************
+	*	Start Next State		*
+	************************/
+	$(".column-details .barchart-bars").on("click", "[action = 'start-next']", function()	{
+		$(this).prev().removeClass("active");
+		$(this).removeClass("inactive").removeClass("next").removeAttr("action").addClass("active");
+		$(this).next().addClass("next").attr("action", "start-next");
+		
+		var date = new Date;
+		var now = addZero(date.getUTCHours()) + ":" + addZero(date.getUTCMinutes());
+		$(this).attr("start", now);
+		
+		$(this).addClass("highlight");
+		
+		var state = $(this).attr("state");
+
+		var print = "Seit " + now +  " · " + states[state-1].title;
+		var legendClass = "text-state-" + state;
+		var target = $(this).closest(".barchart").find(".barchart-legend");
+		target.removeClass().addClass("barchart-legend").addClass(legendClass);
+		target.text(print);
+		
+		var target = $(this).closest(".card-details-container").find(".timestamps").find("input.next");
+		target.attr("value", now).removeClass("next").removeAttr("action");
+		target.parent().parent().next().find("td:last-child input").addClass("next").attr("action", "start-next").attr("value", "Starten");
+	});
+	
+	/************************
+	*	Start Next State		*
+	************************/
+	$(".column-details .timestamps").on("click", "[action = 'start-next']", function()	{
+		$(this).removeClass("next").removeAttr("action");
+		$(this).parent().parent().next().find("td:last-child input").addClass("next").attr("action", "start-next").attr("value", "Starten");
+		
+		var date = new Date;
+		var now = addZero(date.getUTCHours()) + ":" + addZero(date.getUTCMinutes());
+		$(this).attr("value", now);
+		
+		var state = $(this).parent().parent().attr("state");
+				
+		var target = $(this).closest(".column-details").find(".barchart .barchart-bars .next");
+		target.prev().removeClass("active").removeClass("highlight");
+		target.removeClass("inactive").removeClass("next").removeAttr("action").addClass("active").addClass("highlight");
+		target.next().addClass("next").attr("action", "start-next");
+		target.attr("start", now);
+
+		var print = "Seit " + now +  " · " + states[state-1].title;
+		var legendClass = "text-state-" + state;
+		target = $(this).closest(".column-details ").find(".barchart .barchart-legend");
+		target.removeClass().addClass("barchart-legend").addClass(legendClass);
+		target.text(print);
+	});
+
 });
 
 function removeEditability(_target)	{
