65b96d2b97ccee2d28d6ac0d2e58f601bd06ce00
Christoph Labacher <christoph.labacher@gmail.com>
2017-05-25 21:00:08 +0200

    Better use of space

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let win
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({
-    width: 1200,
+    width: 1169,
     height: 760,
     resizable: false,
     webPreferences: {
